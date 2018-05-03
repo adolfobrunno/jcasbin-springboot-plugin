5aad2a9c0d65c6ebe36c56d9ba628eb4deec4ef1
Yang Luo <hsluoyz@qq.com>
2018-05-03 13:07:19 +0800

    Refactor out getUserPassword() in HttpBasicAuthnFilter.

--- src/main/java/org/jcasbin/plugins/HttpBasicAuthnFilter.java
+++ src/main/java/org/jcasbin/plugins/HttpBasicAuthnFilter.java
@@ -35,16 +35,35 @@ public class HttpBasicAuthnFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
-    private boolean checkUserPassword(String username, String password) {
-        return true;
+    @Override
+    public void destroy() {
     }
 
+    // Filters all requests through HTTP basic authentication.
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
 
+        String credentials = getUserPassword(request, response);
+        if (credentials.equals("")) {
+            return;
+        }
+
+        int p = credentials.indexOf(":");
+        String username = credentials.substring(0, p).trim();
+        String password = credentials.substring(p + 1).trim();
+
+        if (!checkUserPassword(username, password)) {
+            unauthorized(response, "Bad credentials");
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    // Gets HTTP basic authentication's user name and password.
+    private String getUserPassword(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String authHeader = request.getHeader("Authorization");
         if (authHeader != null) {
             StringTokenizer st = new StringTokenizer(authHeader);
@@ -57,14 +76,7 @@ public class HttpBasicAuthnFilter implements Filter {
                         LOG.debug("Credentials: " + credentials);
                         int p = credentials.indexOf(":");
                         if (p != -1) {
-                            String username = credentials.substring(0, p).trim();
-                            String password = credentials.substring(p + 1).trim();
-
-                            if (!checkUserPassword(username, password)) {
-                                unauthorized(response, "Bad credentials");
-                            }
-
-                            filterChain.doFilter(servletRequest, servletResponse);
+                            return credentials;
                         } else {
                             unauthorized(response, "Invalid authentication token");
                         }
@@ -74,20 +86,20 @@ public class HttpBasicAuthnFilter implements Filter {
                 }
             }
         } else {
-            unauthorized(response);
+            unauthorized(response, "Authorization header not found");
         }
+
+        return "";
     }
 
-    @Override
-    public void destroy() {
+    // Checks the correctness of user name and password as you like.
+    private boolean checkUserPassword(String username, String password) {
+        return true;
     }
 
+    // Returns HTTP 401 Unauthorized if the authentication fails.
     private void unauthorized(HttpServletResponse response, String message) throws IOException {
         response.setHeader("WWW-Authenticate", "Basic realm=\"" + realm + "\"");
-        response.sendError(401, message);
-    }
-
-    private void unauthorized(HttpServletResponse response) throws IOException {
-        unauthorized(response, "Unauthorized");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, message);
     }
 }
